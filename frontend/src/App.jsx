6bc4574c81c1ad859326525d3f1f544ae890d9f1
isma0417 <ismaiismaisham@gmail.com>
2025-01-30 23:53:34 +0600

    Update front folder

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+import './App.css'
+import Footer from './app/Footer/Footer'
+import Header from './app/Header/Header'
+import Main from './app/Main/Main'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Header/>
+     <Main/>
+     <Footer/>
+    </>
+  )
+}
+
+export default App
